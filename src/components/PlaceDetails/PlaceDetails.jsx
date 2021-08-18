401d74a1427aaa89b19b0bc58c43a97c975907e3
pavlo-osypov-by <pavlo.osypov@edvantis.com>
2021-08-18 12:03:09 +0300

    List & PlaceDetails - preparation to further development

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const PlaceDetails = () => {
+const PlaceDetails = ({ place }) => {
+  console.log('place my: ', place);
+
   return (
-    <h1>PlaceDetails</h1>
+    <h1>{place.name}</h1>
   )
 }
 
